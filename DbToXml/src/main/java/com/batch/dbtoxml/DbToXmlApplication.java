cd8fa2fc6026b4f52e306fd6859a3a169b88436e
akhiakhila17 <91554202+akhiakhila17@users.noreply.github.com>
2023-06-15 11:37:10 +0530

    Add files via upload

---
+++ DbToXml/src/main/java/com/batch/dbtoxml/DbToXmlApplication.java
@@ -0,0 +1,13 @@
+package com.batch.dbtoxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbToXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbToXmlApplication.class, args);
+	}
+
+}
